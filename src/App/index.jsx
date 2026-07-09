6afb1072505a55ef260e28b42f93187b12940ecd
Christian Rojas <christianrojas4000@gmail.com>
2026-07-09 16:24:56 -0600

    terminando de estructurar el proyecto

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -4,11 +4,12 @@
 // import viteLogo from './assets/vite.svg'
 // import heroImg from './assets/hero.png'
 import React from 'react'
-import { TodoCounter } from './TodoCounter'
-import { TodoSearch } from './TodoSearch'
-import { TodoList } from './TodoList'
-import { TodoItem } from './TodoItem'
-import { CreateTodoButton } from './CreateTodoButton'
+import { TodoCounter } from '../TodoCounter'
+import { TodoSearch } from '../TodoSearch'
+import { TodoList } from '../TodoList'
+import { TodoItem } from '../TodoItem'
+import { CreateTodoButton } from '../CreateTodoButton'
+import { useLocalStorage } from './useLocalStorage'
 
 // const defaultTodos = [
 //   { text: 'Cortar Cebolla', completed: true },
@@ -23,25 +24,7 @@ import { CreateTodoButton } from './CreateTodoButton'
 
 // localStorage.removeItem('TODOS_V1')
 
-function useLocalStorage(itemName, initialValue) {
-  const localStorageItem = localStorage.getItem(itemName)
-  let parsedItem;
-  if (!localStorageItem) {
-    localStorage.setItem(itemName, JSON.stringify(initialValue))
-    parsedItem = initialValue
-  } else {
-    parsedItem = JSON.parse(localStorageItem)
-  }
-  const [item, setItem] = React.useState(parsedItem)
 
-  const saveItem = (newItem) => {
-    localStorage.setItem(itemName, JSON.stringify(newItem))
-    setItem(newItem)
-
-  }
-  return [item,saveItem]
-
-}
 
 function App() {
   const [todos, saveTodos] = useLocalStorage('TODOS_V1', [])
